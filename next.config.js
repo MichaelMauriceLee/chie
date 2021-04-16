13a61981cfda82327b84967cfe9d42334a79362d
Michael Lee <m.maurice.lee@gmail.com>
2021-04-16 08:22:55 -0600

    Fixed PWA

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
+  future: {
+    webpack5: true,
+  },
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
