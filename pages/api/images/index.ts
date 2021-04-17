a1047609e832e8d4516a5ec216a1450aa7169e0e
Michael Lee <m.maurice.lee@gmail.com>
2021-04-17 17:38:53 -0600

    analyze image works

--- pages/api/images/index.ts
+++ pages/api/images/index.ts
@@ -1,10 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
+import getRawBody from 'raw-body';
 
 const analyzeImageUrl = `https://${process.env.CV_NAME}.cognitiveservices.azure.com/vision/v3.2-preview.3/read/analyze?language=ja`;
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 const images = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  const { headers } = await axios.post(analyzeImageUrl, req.body, {
+  const buffer = await getRawBody(req);
+  const { headers } = await axios.post(analyzeImageUrl, buffer, {
     headers: {
       'Ocp-Apim-Subscription-Key': process.env.CV_KEY,
       'Content-Type': 'application/octet-stream',
