ac7aecfcfcf6301b5ed88ba5787a51f05f9ee920
Michael Lee <m.maurice.lee@gmail.com>
2021-04-18 19:17:52 -0600

    Working storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,11 @@
 module.exports = {
   "stories": [
-    "../src/stories/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    '@storybook/addon-postcss',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
+    "@storybook/addon-essentials"
   ],
   "core": {
     "builder": "webpack5"
