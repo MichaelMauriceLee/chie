a7f0dc59d6fe16d9e67eeb309bf831ab44a7f0fb
Michael Lee <m.maurice.lee@gmail.com>
2024-12-28 00:30:17 -0700

    Fix dark mode on initial load

--- components/app-initializer/index.tsx
+++ components/app-initializer/index.tsx
@@ -5,16 +5,19 @@ import { useSetAtom } from "jotai";
 import {
   initializeDeckNamesAtom,
   initializeSelectedDeckAtom,
+  initializeThemeAtom,
 } from "@/store/atoms";
 
 export default function AppInitializer() {
   const hydrateDeckNames = useSetAtom(initializeDeckNamesAtom);
   const hydrateSelectedDeck = useSetAtom(initializeSelectedDeckAtom);
+  const hydrateTheme = useSetAtom(initializeThemeAtom);
 
   useEffect(() => {
     hydrateDeckNames();
     hydrateSelectedDeck();
-  }, [hydrateDeckNames, hydrateSelectedDeck]);
+    hydrateTheme();
+  }, [hydrateDeckNames, hydrateSelectedDeck, hydrateTheme]);
 
   return null;
 }
--- store/atoms.ts
+++ store/atoms.ts
@@ -1,39 +1,44 @@
 import { atom } from "jotai";
 
-function getInitialTheme(): "light" | "dark" {
-  if (typeof window !== "undefined") {
-    const storedTheme = localStorage.getItem("theme") as "light" | "dark";
-    if (storedTheme) return storedTheme;
-
-    const prefersDark = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-    return prefersDark ? "dark" : "light";
-  }
-
-  return "light"; 
-}
-
-export const themeAtom = atom<"light" | "dark">(getInitialTheme());
+export const themeAtom = atom<"light" | "dark">("light");
 
 export const deckNamesAtom = atom<string[]>([]);
 
 export const selectedDeckAtom = atom<string>("");
 
+export const initializeThemeAtom = atom(
+  (get) => get(themeAtom),
+  (_get, set) => {
+    if (typeof window !== "undefined") {
+      const storedTheme = localStorage.getItem("theme") as "light" | "dark";
+      const prefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+
+      const theme = storedTheme || (prefersDark ? "dark" : "light");
+      set(themeAtom, theme);
+    }
+  }
+);
+
 export const initializeDeckNamesAtom = atom(
   (get) => get(deckNamesAtom),
   (_get, set) => {
-    const storedDeckNames = JSON.parse(
-      localStorage.getItem("deckNames") || "[]"
-    );
-    set(deckNamesAtom, storedDeckNames);
+    if (typeof window !== "undefined") {
+      const storedDeckNames = JSON.parse(
+        localStorage.getItem("deckNames") || "[]"
+      );
+      set(deckNamesAtom, storedDeckNames);
+    }
   }
 );
 
 export const initializeSelectedDeckAtom = atom(
   (get) => get(selectedDeckAtom),
   (_get, set) => {
-    const storedSelectedDeck = localStorage.getItem("selectedDeck") || "";
-    set(selectedDeckAtom, storedSelectedDeck);
+    if (typeof window !== "undefined") {
+      const storedSelectedDeck = localStorage.getItem("selectedDeck") || "";
+      set(selectedDeckAtom, storedSelectedDeck);
+    }
   }
 );
